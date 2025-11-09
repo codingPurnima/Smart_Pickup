62bd2ccab9a87250832a15030f27b749c7c38d96
Purnima Pandey <purnimapnd04@gmail.com>
2025-11-09 16:10:41 +0530

    Added remaining files

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:smart_pickup_system/screens/parent_dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ParentDashboard(parentId: "Temporary id",),
+    );
+  }
+}
